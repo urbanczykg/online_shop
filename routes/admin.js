9dc862885ea7dc550dba6ef2eacf06047b6f1a3e
Gabriela <gabriela.urbanczyk2000@gmail.com>
2021-02-20 22:09:36 +0100

    html files

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 
 
-router.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</form>');
+router.get('/add-product',(req,res,next)=>{
+    res.send('<form action="admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</form>');
 });
 router.post('/product',(req,res,next)=>{
     console.log(req.body)
--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send('<h1>Hello expresja</h1>');
+    res.sendFile('/views/shop.html');
        next();
    });
 
