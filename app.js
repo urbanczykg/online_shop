cbf7dd05103706ba7cf05becb55012e6026eaff7
Gabriela Urbańczyk <gabriela@aexol.com>
2021-02-22 22:53:14 +0100

    new eror and views mod

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const http=require('http');
-const bodyParser=require('body-parser');
-const express=require('express');
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
 const adminRoutes = require('./routes/admin');
-const shopRoutes= require('./routes/shop');
-const app=express();
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('admin', adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1> Page not found </h1>');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views','error.html'))
 });
+
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,9 @@
-const patch = require('path');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-   res.sendFile(patch.join(__dirname, '../', 'views', 'shop.html' ));
+   res.sendFile(path.join(__dirname, '../', 'views', 'shop.html' ));
     
 });
 
