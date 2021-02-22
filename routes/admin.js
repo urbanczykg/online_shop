92bf3c49b7df2e384db7bc57f8f62a3c909dec2f
Gabriela <gabriela.urbanczyk2000@gmail.com>
2021-02-22 09:30:57 +0100

    prettier

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 
 
-router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</form>');
+router.get('/add-product', (req, res, next)=>{
+    res.send('<form action = "admin/add-product" method = "POST" > <input type = "text" name = "title" > < button type = "submit" >add product</form>');
 });
-router.post('/product',(req,res,next)=>{
+router.post('/product', (req, res, next)=>{
     console.log(req.body)
     res.redirect('/');
 });
 
-module.exports =router;
+module.exports = router;
