6bff315b378b75f0d1e21c9d041b7f4c598a4d4d
Gabriela Urbańczyk <gabriela@aexol.com>
2021-02-22 21:30:37 +0100

    linux fix

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-router.get('/add-product', (req, res, next)=>{
-    res.send('<form action = "admin/add-product" method = "POST" > <input type = "text" name = "title" > < button type = "submit" >add product</form>');
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action = "admin/add-product" method = "POST" > <input type = "text" name = "title" > < button type = "submit" >add product</form>'
+  );
 });
-router.post('/product', (req, res, next)=>{
-    console.log(req.body)
-    res.redirect('/');
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
+elo;
 
 module.exports = router;
