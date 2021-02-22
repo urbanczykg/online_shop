d386de063defe243977965b947f4e12ce8b74ea1
Gabriela <gabriela.urbanczyk2000@gmail.com>
2021-02-22 21:05:46 +0100

    change js file

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 
-
 router.get('/add-product', (req, res, next)=>{
     res.send('<form action = "admin/add-product" method = "POST" > <input type = "text" name = "title" > < button type = "submit" >add product</form>');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const patch = require('path');
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-    res.sendFile('/views/shop.html')
-       next();
-   });
+router.get('/', (req, res, next)=>{
+   res.sendFile(patch.join(__dirname, '../', 'views', 'shop.html' ));
+    
+});
 
    module.exports=router
    
